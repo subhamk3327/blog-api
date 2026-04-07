6f16a1addc55f9ba75a7d3c5187d1f2819985bfc
subhamk3327 <subhamkumardinesh@gmail.com>
2026-04-07 14:24:41 +0530

    added new features and removed bugs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 '''blog api'''
 import psycopg2
+from psycopg2 import errors
 from fastapi import FastAPI, HTTPException, Depends
 from jose import JWTError, jwt
 import os
@@ -36,11 +37,6 @@ class user_c(BaseModel):
 
 class comments_c(BaseModel):
     message : str
-    username : str
-
-class likes_c(BaseModel):
-    likes : int
-    username : str
 
 #POSTS
 @app.get("/post")#since this is a blog posts are public
@@ -95,7 +91,7 @@ def delete_posts(post_id: int,token : str = Depends(oauth_scheme)):
     conn.close()
     return{"message": "post deleted successfully"}
 
-@app.put("/post/{post_id}")#will add crash check, if exists already, verifications and jwt later
+@app.put("/post/{post_id}")#Only creator can edit posts
 def change_post(post_id : int, new_post : post_c,token : str = Depends(oauth_scheme)):
     try:
         payload = jwt.decode(token,secret_key,algorithms=[algo])
@@ -119,20 +115,27 @@ def change_post(post_id : int, new_post : post_c,token : str = Depends(oauth_sch
     return {"message": "post edited successfully"}
 
 #COMMENTS
-@app.post("/post/{post_id}/comments")
-def post_comments(new_comment: comments_c, post_id : int):
+@app.post("/post/{post_id}/comments")#Only registered users can comment
+def post_comments(new_comment: comments_c, post_id : int, token : str = Depends(oauth_scheme)):
+    try:
+        payload = jwt.decode(token,secret_key,algorithms=[algo])
+        username = payload.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="invalid token")
     conn = get_db()
     cursor = conn.cursor()
+    cursor.execute("select * from users where username=%s",(username,))
+    fetch_user = cursor.fetchone()
     cursor.execute("select * from post where id =%s",(post_id,))
     value = cursor.fetchone()
     if value is None:
         raise HTTPException(status_code=404, detail="post not found to register comment")
-    cursor.execute("insert into comments(post_id,username,comment) values(%s,%s,%s)",(post_id, new_comment.username,new_comment.message))
+    cursor.execute("insert into comments(post_id,user_id,comment) values(%s,%s,%s)",(post_id, fetch_user[0],new_comment.message))
     conn.commit()
     conn.close()
     return {"message": "comment posted successfully"}
 
-@app.get("/post/{post_id}/comments")
+@app.get("/post/{post_id}/comments")#comments are public too
 def get_comments(post_id:int):
     conn = get_db()
     cursor = conn.cursor()
@@ -143,37 +146,57 @@ def get_comments(post_id:int):
     cursor.execute("select * from comments WHERE post_id=%s",(post_id,))
     rows= cursor.fetchall()
     conn.close()
-    return [{"id": row[0],"post_id" : row[4],"username": row[1], "comment":row[2]} for row in rows]
+    return [{"id": row[0],"post_id" : row[4],"comment": row[1], "user_id":row[2]} for row in rows]
 
-@app.delete("/post/{post_id}/comments/{comment_id}")
-def delete_comments(post_id:int,comment_id:int):
+@app.delete("/post/{post_id}/comments/{comment_id}")#can only be deleted by post creator or comment creator
+def delete_comments(post_id:int,comment_id:int, token : str = Depends(oauth_scheme)):
+    try:
+        payload = jwt.decode(token,secret_key,algorithms=[algo])
+        username = payload.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="invalid token")
     conn = get_db()
     cursor = conn.cursor()
+    cursor.execute("select * from users where username=%s",(username,))
+    fetch_user= cursor.fetchone()
     cursor.execute("select * from post where id=%s",(post_id,))
-    value= cursor.fetchone()
-    if value is None:
+    post= cursor.fetchone()
+    if post is None:
         raise HTTPException(status_code=404,detail="post id not found")
     cursor.execute("select * from comments where id=%s",(comment_id,))
-    value= cursor.fetchone()
-    if value is None:
+    comment= cursor.fetchone()
+    if comment is None:
         raise HTTPException(status_code=404,detail="comment under this post_id not found")
+    validation = comment[2]==fetch_user[0] or post[3]==fetch_user[0]
+    if not validation:
+        raise HTTPException(status_code=401,detail="not allowed to delete others comment")
     cursor.execute("delete from comments where id=%s",(comment_id,))
     conn.commit()
     conn.close()
     return {"message":"comment deleted under that post"}
 
-@app.put("/post/{post_id}/comments/{comment_id}")
-def change_comment(post_id:int,comment_id:int,new_comment:comments_c):
+@app.put("/post/{post_id}/comments/{comment_id}")#can only be posted by registered users
+def change_comment(post_id:int,comment_id:int,new_comment:comments_c,token : str = Depends(oauth_scheme)):
+    try:
+        payload = jwt.decode(token,secret_key,algorithms=[algo])
+        username = payload.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="invalid token")
     conn = get_db()
     cursor = conn.cursor()
+    cursor.execute("select * from users where username=%s",(username,))
+    fetch_user= cursor.fetchone()
     cursor.execute("select * from post where id=%s",(post_id,))
-    value= cursor.fetchone()
-    if value is None:
+    fetch_post= cursor.fetchone()
+    if fetch_post is None:
         raise HTTPException(status_code=404,detail="post id not found")
     cursor.execute("select * from comments where id=%s",(comment_id,))
-    value= cursor.fetchone()
-    if value is None:
+    fetch_comment= cursor.fetchone()
+    if fetch_comment is None:
         raise HTTPException(status_code=404,detail="comment under this post_id not found")
+    validation = fetch_comment[2]==fetch_user[0]
+    if not validation:
+        raise HTTPException(status_code=401,detail="not allowed to delete others comment")
     cursor.execute("update comments set comment=%s where id=%s",(new_comment.message,comment_id))
     conn.commit()
     conn.close()
@@ -181,22 +204,27 @@ def change_comment(post_id:int,comment_id:int,new_comment:comments_c):
 
 # LIKES
 @app.post("/post/{post_id}/likes")#need to update user_id mechanism better way
-def post_likes(post_id:int,user_id : int):
+def post_likes(post_id:int,token : str = Depends(oauth_scheme)):
+    try:
+        payload = jwt.decode(token,secret_key,algorithms=[algo])
+        username = payload.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="invalid token")
     conn = get_db()
     cursor = conn.cursor()
     cursor.execute("select * from post where id=%s",(post_id,))
-    value= cursor.fetchone()
-    if value is None:
+    fetch_post= cursor.fetchone()
+    if fetch_post is None:
         raise HTTPException(status_code=404,detail="post id not found")
-    cursor.execute("select * from likes where post_id=%s",(post_id,))
-    value_check= cursor.fetchone()
-    if value_check is None:
-        cursor.execute("insert into likes(like_count,post_id,user_id) values(%s,%s,%s)",(1,post_id,user_id))
-        conn.commit()
-    elif value_check is not None:
-        cursor.execute("update likes set like_count=like_count+1, user_id=%s where post_id=%s",(user_id,post_id))
-        conn.commit()
+    cursor.execute("select * from users where username=%s",(username,))
+    fetch_user= cursor.fetchone()
+    try:
+        cursor.execute("insert into likes(post_id,user_id) values(%s,%s)",(fetch_post[0],fetch_user[0]))
+    except errors.UniqueViolation:
         conn.close()
+        raise HTTPException(status_code=400,detail="you already likes this post")
+    conn.commit()
+    conn.close()
     return {"message":"liked this post successfully"}
 
 @app.get("/post/{post_id}/likes")
@@ -207,38 +235,51 @@ def get_likes(post_id:int):
     value= cursor.fetchone()
     if value is None:
         raise HTTPException(status_code=404,detail="post id not found")
-    cursor.execute("select * from likes where post_id=%s",(post_id,))
-    rows=cursor.fetchall()
+    cursor.execute("select count(*) from likes where post_id=%s",(post_id,))
+    likes=cursor.fetchone()
     conn.close()
-    return [{"like_id":row[0],"post_id": row[2], "likes":row[1],"user_id":row[3]}for row in rows]
+    return {"likes on this post":likes[0]}
 
 @app.delete("/post/{post_id}/likes")#currently anyone can delete we add userbased deletion later
-def delete_likes(post_id: int,):
+def delete_likes(post_id: int,token : str = Depends(oauth_scheme)):
+    try:
+        payload = jwt.decode(token,secret_key,algorithms=[algo])
+        username = payload.get("sub")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="invalid token")
     conn = get_db()
     cursor = conn.cursor()
+    cursor.execute("select * from users where username=%s",(username,))
+    fetch_user= cursor.fetchone()
     cursor.execute("select * from post where id=%s",(post_id,))
-    value= cursor.fetchone()
-    if value is None:
+    fetch_post= cursor.fetchone()
+    if fetch_post is None:
         raise HTTPException(status_code=404,detail="post id not found")
-    cursor.execute("select * from likes where post_id=%s",(post_id,))
-    values = cursor.fetchone()
-    cursor.execute("update likes set like_count= like_count-1 where post_id=%s",(post_id,))
+    cursor.execute("select * from likes where post_id=%s and user_id=%s",(post_id,fetch_user[0]))
+    fetch_one_like = cursor.fetchone()
+    if fetch_one_like is None:
+        raise HTTPException(status_code=404,detail="user did not like the post")
+    cursor.execute("delete  from likes where id=%s",(fetch_one_like[0],))
     conn.commit()
     conn.close()
     return {"message": "like delted under this post successfully"}
 
-@app.post("/register")#will add unique userid later
+@app.post("/register")#unique username
 def registration(user : user_c):
     conn = get_db()
     cursor = conn.cursor()
     user.password=pwd_context.hash(user.password)
-    cursor.execute("insert into users(username,password) values(%s,%s)",(user.username,user.password))
+    try:
+        cursor.execute("insert into users(username,password) values(%s,%s)",(user.username,user.password))
+    except errors.UniqueViolation:
+        conn.close()
+        raise HTTPException(status_code=400, detail="username already exists")
     conn.commit()
     conn.close()
     return {"sucess":"user registered"}
 
     
-@app.post("/login")
+@app.post("/login")#jwt auth with time expiry
 def login(user : user_c):
     conn = get_db()
     cursor = conn.cursor()
